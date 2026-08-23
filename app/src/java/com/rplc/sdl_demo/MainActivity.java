d20b85b457504ef4f7766182ffe573b23b0c3f16
RandomPerson <bagas.gwlt@gmail.com>
2026-08-23 18:01:00 +0700

    Feature: migrate to Meson for C++ compilation

--- app/src/java/com/rplc/sdl_demo/MainActivity.java
+++ app/src/java/com/rplc/sdl_demo/MainActivity.java
@@ -8,7 +8,6 @@ public class MainActivity extends SDLActivity {
   @Override
   protected String[] getLibraries() {
     return new String[] {
-      "c++_shared",
       "SDL3",
       "main"
     };
