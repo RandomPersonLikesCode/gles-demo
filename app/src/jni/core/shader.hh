3631c7205898dec63468368e744f2bf6c9275ff3
RandomPerson <bagas.gwlt@gmail.com>
2026-08-24 09:34:27 +0700

    Refactor: cleanup shader creation

--- app/src/jni/core/shader.hh
+++ app/src/jni/core/shader.hh
@@ -5,12 +5,12 @@
 #include <GLES3/gl32.h>
 
 namespace Core {
+    bool load_shader(const char *path, GLenum type, GLuint &out);
+
     struct Program {
         GLuint handle = 0;
 
         bool create(void);
         ~Program(void);
-
-        static bool load_shader(const char *path, GLenum type, GLuint *out);
     };
 }
